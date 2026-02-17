886043fdd3355cb683e033e68eeccdfb4cac583f
mindy-h <melindastl@gmail.com>
2026-02-17 17:11:42 -0600

    Change base path based on NODE_ENV

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/bud/',
+  base: process.env.NODE_ENV === 'production' ? '/bud/' : '/',
   plugins: [react()],
   build: {
     cssMinify: 'lightningcss',
